b21af0db810936f327d95fc011087f1ab742060f
amitje <amitpaltan54@gmail.com>
2018-10-02 00:05:26 -0400

    add

---
+++ src/seleniumframework/practise.java
@@ -0,0 +1,15 @@
+package seleniumframework;
+
+public class practise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int a=4;
+		int b=6;
+		System.out.println(a=b);
+	}
+
+}
